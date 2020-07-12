b5ec1f7902a73d3f33fec287fc47fafe6931d0d6
Fabricio Oliveira <oliveira.fabricio.santos@gmail.com>
2020-07-12 17:03:10 -0300

    Atualização na classe principal, apresentando o resultado do agendamento.

--- src/main/java/com/fabricio/jobscheduler/JobschedulerApplication.java
+++ src/main/java/com/fabricio/jobscheduler/JobschedulerApplication.java
@@ -1,11 +1,15 @@
 package com.fabricio.jobscheduler;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fabricio.jobscheduler.business.JobSchedulerBusiness;
 import com.fabricio.jobscheduler.service.FileService;
 
 @SpringBootApplication
@@ -13,6 +17,8 @@ public class JobschedulerApplication implements CommandLineRunner {
 
 	@Autowired
 	private FileService fileService;
+	@Autowired
+	private JobSchedulerBusiness jobSchedulerBusiness;
 	
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(JobschedulerApplication.class);
@@ -27,7 +33,17 @@ public class JobschedulerApplication implements CommandLineRunner {
 		String firstLine = fileService.readFile("input.txt", 1, 1);
 		String json = fileService.readFile("input.txt", 2, 0);
 		
+		List<LocalDateTime> schedulePeriod = jobSchedulerBusiness.findSchedulePeriod(firstLine);
 		
+		LocalDateTime startExecutionDateTime = schedulePeriod.get(0);
+		LocalDateTime finishExecutionDateTime = schedulePeriod.get(1);
+		Integer maxExecutionTime = 8;
+		
+		List<List<Long>> scheduledJobs = jobSchedulerBusiness.schedule(json, startExecutionDateTime, finishExecutionDateTime, maxExecutionTime);
+		
+		fileService.writeFile(scheduledJobs.toString(), "output.txt");
+		
+		System.out.println(scheduledJobs);
 	}
 
 }
