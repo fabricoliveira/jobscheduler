711a12a85ace1c0ba53eedeee71ac53ee3db0095
Fabricio Oliveira <oliveira.fabricio.santos@gmail.com>
2020-07-12 15:14:56 -0300

    Definição do método run, para rodar como Java Application

--- src/main/java/com/fabricio/jobscheduler/JobschedulerApplication.java
+++ src/main/java/com/fabricio/jobscheduler/JobschedulerApplication.java
@@ -1,13 +1,22 @@
 package com.fabricio.jobscheduler;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobschedulerApplication {
+public class JobschedulerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobschedulerApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(JobschedulerApplication.class);
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.run(args);
+	}
+
+	
+	@Override
+	public void run(String... args) throws Exception {
 	}
 
 }
